e80029964e9ed1d1c23827a8403a8dfb5f991df2
EternalZero <jray12332@gmail.com>
2016-04-28 17:34:58 -0700

    Jairo: Testing AJAX properties(I tampered maing.go, index.html, and added main.js in assets
    should work if hosted localy, but idk about raw git,
    Also, the submit box is located under ferret on Missions..I put it their
    for testing purposes

--- main.go
+++ main.go
@@ -1,83 +1,66 @@
 package myapp
 
+
 import (
-	//"golang.org/x/net/context"
-	"net/http"
-	//"google.golang.org/cloud/storage"
-	//"io"
 	"html/template"
+	"net/http"
+	"encoding/json"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
 const gcsBucket = "csci-130group.appspot.com"
 
-
-//we could use this to pass it to our functions
-//that way we don't have to have too many parameters
-//NOT SURE IF WE WILL USE THIS THOUGH
-//type demo struct{
-//	ctx	context.Context
-//	w	http.ResponseWriter
-//	bucket	*storage.BucketHandle
-//	client 	*storage.Client
-//}
+type Word struct {
+	Name string
+}
 
 var tpl* template.Template
 
 func init(){
-	tpl = template.Must(tpl.ParseGlob("templates/*.html"))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/check", wordCheck)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("assets/"))))
 
-
-	http.HandleFunc("/", homePage)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-
-
-	http.ListenAndServe(":8080",nil)
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-func homePage(res http.ResponseWriter, req* http.Request){
-	//if req.URL.Path != "/" {
-	//	http.NotFound(res, req)
-	//	return
-	//}
+func index(res http.ResponseWriter, req* http.Request){
+	if req.Method == "POST" {
+
+		var w Word
+		w.Name = req.FormValue("new-word")
+
+		ctx := appengine.NewContext(req)
+		log.Infof(ctx, "WORD SUBMITTED: %v", w.Name)
+
+		key := datastore.NewKey(ctx, "Dictionary", w.Name, 0, nil)
+		_, err := datastore.Put(ctx, key, &w)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 	tpl.ExecuteTemplate(res, "index.html", nil)
 }
 
-//func putFile(ctx context.Context, name string, rdr io.Reader) error {
-//
-//	client, err := storage.NewClient(ctx)
-//	if err != nil {
-//		return err
-//	}
-//	defer client.Close()
-//
-//	writer := client.Bucket(gcsBucket).Object(name).NewWriter(ctx)
-//
-//	io.Copy(writer, rdr)
-//	// check for errors on io.Copy in production code!
-//	return writer.Close()
-//}
-//
-//func getFile(ctx context.Context, name string) (io.ReadCloser, error) {
-//	client, err := storage.NewClient(ctx)
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer client.Close()
-//
-//	return client.Bucket(gcsBucket).Object(name).NewReader(ctx)
-//}
-//
-//func getFileLink(ctx context.Context, name string) (string, error) {
-//	client, err := storage.NewClient(ctx)
-//	if err != nil {
-//		return "", err
-//	}
-//	defer client.Close()
-//
-//	attrs, err := client.Bucket(gcsBucket).Object(name).Attrs(ctx)
-//	if err != nil {
-//		return "", err
-//	}
-//	return attrs.MediaLink, nil
-//}
+
+func wordCheck(res http.ResponseWriter, req *http.Request) {
+
+	ctx := appengine.NewContext(req)
+
+	// acquire the incoming word
+	var w Word
+	json.NewDecoder(req.Body).Decode(&w)
+	log.Infof(ctx, "ENTERED wordCheck - w.Name: %v", w.Name)
+
+	// check the incoming word against the datastore
+	key := datastore.NewKey(ctx, "Dictionary", w.Name, 0, nil)
+	err := datastore.Get(ctx, key, &w)
+	if err != nil {
+		json.NewEncoder(res).Encode("false")
+		return
+	}
+	json.NewEncoder(res).Encode("true")
+}
