b92e2b736f97f33a058b89dfdadb544dfac08c52
Matt Morado <matthewmorado@gmail.com>
2016-05-07 16:33:26 -0700

    Matt: fixed some of Roberto's shit

--- main.go
+++ main.go
@@ -39,7 +39,8 @@ func init(){
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/changepass", changePass)
-	http.HandleFunc("/files",files)
+	http.HandleFunc("/files", files)
+	http.HandleFunc("/photos", gallery)
 	http.HandleFunc("/api/check", wordCheck)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
@@ -169,16 +170,23 @@ func changePass(res http.ResponseWriter, req *http.Request){
 }
 
 func files(res http.ResponseWriter, req *http.Request){
+	cookie := genCookie(res, req)
 	if req.Method == "POST" {
 		src, hdr, err := req.FormFile("data")
 		if err != nil{
 			log.Println("error uploading photo: ", err)
 		}
-		cookie = uploadPhoto(src, hdr, cookie)
+		cookie = uploadPhoto(src, hdr, cookie, req)
 		http.SetCookie(res, cookie)
 	}
 	m := Model(cookie)
 	tpl.ExecuteTemplate(res, "files.html", m)
+}
+
+func gallery(res http.ResponseWriter, req *http.Request){
+	cookie := genCookie(res, req)
+	m := Model(cookie)
+	tpl.ExecuteTemplate(res, "gallery.html", m)
 }
 
 func wordCheck(res http.ResponseWriter, req *http.Request) {
